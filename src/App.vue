800662dae3f96c2d541a7ea79c644c4787660d2c
Ebraim30 <ebraim.herrera@gmail.com>
2023-03-20 11:53:04 -0300

    AA

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,50 @@
 <script setup>
-    const nome = 'Ebraim'
-    const idade = 14
 
-    function inverter(texto) {
-        return texto.split('').reverse().join('').toUpperCase()
-    }
+import { ref, onMounted } from 'vue'
 
-    function saudacao() {
-        return `Olá, ${nome}!`
-    }
+const contador = ref(0)
+const contador2 = ref(0)
+
+function incrementarContador() {
+  contador.value++
+}
+function decrementarContador() {
+  contador.value--
+}
+function incrementarContador2() {
+  contador2.value++
+}
+function decrementarContador2() {
+  contador2.value--
+}
+onMounted(() => {
+  console.log(`O valor inicial do contador é ${contador.value}.`)
+}) 
+    
 </script>
 
 <template>
-    <div class="info">
-        <h1>Exemplo 1 - Mostrando informações na tela</h1>
-        <p>Nome: {{ nome }} </p>
-        <p>Idade: {{ idade }}  anos</p>
-        <hr />
-        <p>Para completar 15 anos faltam: {{ 50 - idade }}  anos</p>
-        <p>O nome tem {{ nome.length }}  caracteres</p>
-        <p>O nome invertido é: {{ inverter(nome) }} </p>
-        <hr />
-        <p> Exemplo de saudação usando função</p>
-        <p> {{ saudacao() }}  </p>
+    <div>
+    <div> <button @click="incrementarContador">+</button>
+    <button @click="decrementarContador">-</button>
+    <p>Valor do contador é: {{ contador }}</p>
+     </div>  
+     
+     <div>
+        <p>+</p>
+     </div>
+
+     
+    <div><button @click="incrementarContador2">+</button>
+    <button @click="decrementarContador2">-</button>
+    <p>Valor do contador é: {{ contador2 }}</p>
     </div>
+    </div>
+
+    <p>resultado = {{ contador + contador2 }}</p>
+
 </template>
 
 <style scoped>
-.info {
-  background-color: rgba(35,12,22,1);
-  padding: 20px 30px;
-  color: rgb(183, 210, 219);
-  border-radius: 10px;
-}
 
-h1 {
-  color: white;
-  font-weight: bold;
-  font-size: 1.5em;
-}
 </style>
